c90663d11bf01affeaf8d8c0c53d21e05c80981b
dewnoi <kittitat955@gmail.com>
2025-10-30 20:13:57 +0700

    [mod]:mod test case

--- backend/test/e2e/auth.e2e-spec.ts
+++ backend/test/e2e/auth.e2e-spec.ts
@@ -17,9 +17,9 @@ describe('Auth E2E (Backend)', () => {
 
   it('create account should return body', async () => {
     const res = await request(app.getHttpServer())
-      .post('/auth/signin')
-      .send({ email: 'test@example.com', password: '123456' })
-      .expect(401);
+      .post('/auth/signup')
+      .send({ email: 'test@gmail.com', password: '1234' })
+      .expect(201);
 
     expect(res.body).toBeDefined();
   });
@@ -27,7 +27,7 @@ describe('Auth E2E (Backend)', () => {
   it('log in with wrong credential should not return token', async () => {
     const res = await request(app.getHttpServer())
       .post('/auth/signin')
-      .send({ email: 'test@example.com', password: '123456' })
+      .send({ email: 'test@gmail.com', password: '123' })
       .expect(401);
 
     expect(res.body.access_token).toBeUndefined();
@@ -36,7 +36,7 @@ describe('Auth E2E (Backend)', () => {
   it('log in with correct credential should return token', async () => {
     const res = await request(app.getHttpServer())
       .post('/auth/signin')
-      .send({ email: 'racha@gmail.com', password: '1234' })
+      .send({ email: 'test@gmail.com', password: '1234' })
       .expect(201);
 
     expect(res.body.access_token).toBeDefined();
