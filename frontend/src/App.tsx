16ca4f6dabca008a37b8ba72e13fe2efb0856b17
Ingkasurat <Ingkasurat2@gmail.com>
2025-11-09 19:27:36 +0700

    Create Edit, but something wrong with style i guess, i will fix it soon

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,20 @@
-import { Route, Routes, useNavigate } from 'react-router-dom'
+import { Route, Routes, useNavigate, useLocation } from 'react-router-dom'
 import './App.css'
 import { Suspense, useEffect, useState } from 'react'
 import Loading from './pages/Loading'
-import { ADMIN_DASHBOARD_PATH, BASE_PATH, NOT_FOUND_PATH, PROFILE_INFO_PATH, SIGN_IN_PATH, SIGN_UP_PATH } from './constants/routes'
+import { ADMIN_DASHBOARD_PATH, BASE_PATH, NOT_FOUND_PATH, PROFILE_INFO_PATH,
+         SIGN_IN_PATH, SIGN_UP_PATH,
+         SECTION_CREATE_PATH, 
+         ITEM_CREATE_PATH, ITEM_EDIT_PATH } from './constants/routes'
 import { toast, Toaster } from 'sonner'
 import Home from './pages/Home/Home'
 import Profile from './pages/Profile/Profile'
 import SignIn from './pages/Auth/SignIn'
 import SignUp from './pages/Auth/SignUp'
 import AdminDashBoard from './pages/Admin/DashBoard'
+import CreateSectionPage from './pages/Sections/CreateSectionPage'
+import CreateItemModal from './pages/Item/CreateItemModal'
+import EditItemPage from './pages/Item/EditItemPage'
 import NotFound from './pages/NotFound'
 import { axiosInstance } from './lib/axios'
 import ServerDown from './pages/ServerDown'
@@ -47,7 +53,11 @@ function App() {
   }, [])
 
   if (isServerDown) return <ServerDown />
-  
+
+  const location = useLocation()
+  const state = location.state as { background?: Location } | undefined
+  const background = state?.background
+
   return (
     <Suspense fallback = {<Loading/>} >
       <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
@@ -55,28 +65,38 @@ function App() {
         <Toaster />
           <div className = 'flex flex-col min-h-screen'>
               <main className = "flex-1">
+                <Routes location={background || location}>
+                    <Route path = {BASE_PATH} element = {<Home/>} />
+              
+                    <Route path = {PROFILE_INFO_PATH} element = {<Profile />}>
+                      <Route path="section/new" element={<CreateSectionPage />} />
+                    </Route>
+              
+                    <Route path = {SIGN_IN_PATH} element={<SignIn />} />
+                    <Route path = {SIGN_UP_PATH} element={<SignUp />} /> 
+              
+                    {/* Admin */}
+                    <Route element = {<AdminProtected />}>
+                      <Route path = {ADMIN_DASHBOARD_PATH} element={<AdminDashBoard />} />         
+                    </Route>
+
+                    <Route path={SECTION_CREATE_PATH} element={<CreateSectionPage />} />
+
+                    <Route path={ITEM_EDIT_PATH} element={ <EditItemPage/>} />
+              
+                    <Route path = {NOT_FOUND_PATH} element={<NotFound />} />
+                </Routes>
+
+                {background && (
                   <Routes>
-                      <Route path = {BASE_PATH} element = {<Home/>} />
-                
-                      <Route path = {PROFILE_INFO_PATH} element = {<Profile />} />
-                
-                      <Route path = {SIGN_IN_PATH} element={<SignIn />} />
-                      <Route path = {SIGN_UP_PATH} element={<SignUp />} /> 
-                
-                      {/* Admin */}
-                      <Route element = {<AdminProtected />}>
-                        <Route path = {ADMIN_DASHBOARD_PATH} element={<AdminDashBoard />} />         
-                      </Route>
-                
-                      <Route path = {NOT_FOUND_PATH} element={<NotFound />} />
-                      
+                    <Route path={ITEM_CREATE_PATH} element={<CreateItemModal />} />
                   </Routes>
+                )}
               </main>
           </div>
       </TooltipProvider>
       </GoogleOAuthProvider>
     </Suspense>
-  
   )
 }
 
