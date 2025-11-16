221db42ec2eea31af82dd8c56fb3b220835152fd
dewnoi <kittitat955@gmail.com>
2025-11-16 16:24:27 +0700

    refactor:remove not used vairable

--- frontend/src/pages/Profile/Profile.tsx
+++ frontend/src/pages/Profile/Profile.tsx
@@ -13,8 +13,6 @@ import { CreateSectionForm } from '@/components/Section/components/Form/CreateSe
 import { EditSectionForm } from '@/components/Section/components/Form/EditSectionForm';
 
 const Profile: React.FC = () => {
-  // Fake data for section creation
-  const fakeData = {title: 'Sample Section', description: 'This is a sample section description.', coverMediaId: ''};
 
   const [sectionData, setSectionData] = React.useState<Section[]>([]);
   const [ownerPageId, setOwnerPageId] = React.useState<string | null>(null);
@@ -25,11 +23,6 @@ const Profile: React.FC = () => {
   const [isLoading, setIsLoading] = React.useState<boolean>(true);
 
   const {username} = useParams(); // username of owner profile page
-
-  const containerRef = useRef<HTMLDivElement>(null);
-
-  // Example section ID for deletion test
-  const sectionId = sectionData.length > 0 ? sectionData[0].id : ''; 
 
   const fetchOwnerPageId = async () => {
       try {
@@ -71,102 +64,6 @@ const Profile: React.FC = () => {
     if (ownerPageId) fetchSection();
     setIsLoading(false);
   }, [username, ownerPageId, isLoading]);
-  
- 
-  const handleSubmit = async () => {
-    console.log('Test button clicked');
-    try{
-          const result = await axiosInstance.get('/auth/me');
-          toast.success('Fetch profile successful!');
-          console.log(result.data);
-          console.log(username);
-          
-        }
-        catch (error) {
-          toast.error('An unexpected error occurred');
-          
-        } 
-  }
-  const handleFetchSection = async () => {
-    console.log('Test button clicked');
-    try{
-          const res = await axiosInstance.get(`/section/user/${ownerPageId}` )
-          console.log(res.data);
-          toast.success('Fetch section successful!');
-          
-        }
-        catch (error) {
-          toast.error('An unexpected error occurred');
-          
-        } 
-  
-  }
-  const handleCreateSection = async () => {
-    const body = {
-      ownerId: userId,
-      title: fakeData.title,
-      description: fakeData.description,
-      coverMediaId: fakeData.coverMediaId,
-    };
-    console.log(body);
-    try{
-          const res = await axiosInstance.post(`/section/create`, body )
-          toast.success('Section create!')
-          console.log(res.data);
-        }
-        catch (error) {
-          toast.error('An unexpected error occurred');
-          
-        } 
-  
-  }
-  const handleDeleteSection = async () => {
-    console.log('Test button clicked');
-    try{
-          const res = await axiosInstance.delete(`/section/delete/${sectionId}` )
-          toast.success(res.data.message);
-          
-        }
-        catch (error) {
-          toast.error('An unexpected error occurred');
-          
-        } 
-  
-  }
-
-  const handleOpenCreateForm = async () => {
-    if (ownerPageId!== userId) {
-      toast.error('Youa re not owner of this profile page');
-      return;
-    }
-    console.log('Test button clicked');
-    setIsCreateSectionFormOpen(true);
-    
-  }
-  const handleOpenEditForm = async () => {
-    console.log('Test button clicked');
-    if (ownerPageId!== userId) {
-      toast.error('Youa re not owner of this profile page');
-      return;
-    }
-    setSectionIdToEdit(sectionId);
-    setIsEditSectionFormOpen(true);
-  }
-
-  const handleUpdateSection = async () => {
-    console.log('Test button clicked');
-    const updateData = {
-      title: 'Updated Section Title',
-      description: 'Updated description for the section.',
-    };
-    try{
-          const res = await axiosInstance.patch(`/section/update/${sectionId}`, updateData )
-          toast.success(res.data.message);
-      }
-    catch (error) {
-      toast.error('An unexpected error occurred');
-    }
-  }
 
   return (
     <div className="flex flex-row text-foreground min-h-screen" >
