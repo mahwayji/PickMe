ea17b4ee29e020493c6d316ebfe24ea988626b9d
mahwayji <ham.jirat24@gmail.com>
2025-11-24 20:15:17 +0700

    feat: profile

--- frontend/src/components/utils/MediaToImage.tsx
+++ frontend/src/components/utils/MediaToImage.tsx
@@ -1,19 +1,14 @@
 import { useEffect, useState, type ImgHTMLAttributes } from "react";
 import { axiosInstance } from "@/lib/axios";
 import ImageNotFound from '@/images/ImageNotFound.png'
-import VideoNotFound from '@/images/VideoNotFound.mp4'
 
 
-async function getUrlById(id: string | undefined, type: String) {
+async function getUrlById(id: string | undefined) {
   try {
     const res = await axiosInstance.get(`media/${id}`);
     return res.data as string;
   } catch (error){
-    if(type == 'image')
       return ImageNotFound
-    else if (type == 'video')
-      return VideoNotFound
-    else return ''
   }
 }
 
@@ -26,25 +21,26 @@ export function MediaImage({ mediaId, ...props }: MediaImageProps) {
   const [url, setUrl] = useState("");
 
   useEffect(() => {
-    getUrlById(mediaId, 'image').then(setUrl);
+    getUrlById(mediaId).then(setUrl);
 
   }, [mediaId]);
   return <img src={url} {...props} />;
 }
 
-type MediaVideoProps = {
-  mediaId: string | undefined;
-} & ImgHTMLAttributes<HTMLVideoElement>;
+// type MediaVideoProps = {
+//   mediaId: string | undefined;
+// } & ImgHTMLAttributes<HTMLVideoElement>;
 
-export function MediaVideo({mediaId, ...props}: MediaVideoProps){
-  const [url, setUrl] = useState("")
+// export function MediaVideo({mediaId, ...props}: MediaVideoProps){
+//   const [url, setUrl] = useState("")
 
-  useEffect(() => {
-    getUrlById(mediaId, 'video').then(setUrl);
-  }, [mediaId]);
+//   useEffect(() => {
+//     getUrlById(mediaId, 'video').then(setUrl);
+//   console.log(url)
 
-  if (url === "") 
-    setUrl(ImageNotFound)
-  return <video src={url} {...props} controls/>;
+//   }, [mediaId]);
+
+//   return <video src='https://nest-library-api-mahwayji.s3.ap-southeast-2.amazonaws.com/images/NotFound.png
+// ' {...props} controls/>;
   
-}
+// }
--- frontend/src/pages/Item/Blocks/VideoBlock.tsx
+++ frontend/src/pages/Item/Blocks/VideoBlock.tsx
@@ -1,5 +1,5 @@
-import { MediaVideo } from '@/components/utils/MediaToImage'
 import type { ItemBlock } from '@/types/itemBlock'
+import VideoNotFound from '@/images/VideoNotFound.mp4'
 
 type Props = {
     block: ItemBlock
@@ -7,9 +7,10 @@ type Props = {
 const VideoBlock = ({block}: Props) => {
     if(block.type !== 'video') return (<></>)
     
+    const url = block?.url ? (block.url) : VideoNotFound;
     return (
         <div className = 'p-2'>
-            <MediaVideo mediaId = {block.url} width = {block.width} height = {block.height} className = 'block mx-auto' />
+            <video src = {url} width = {block.width} height = {block.height} className = 'block mx-auto' controls/>
         </div>
     )
 }
